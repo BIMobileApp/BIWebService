79ec3324011a1821b02dd84c05a8f6a2c96669b4
Juthamart <Juthamart.k@v-smart.com>
2018-08-03 16:55:29 +0700

    api for page 'test2'

---
+++ BILibraryBLL/QueryTest.cs
@@ -0,0 +1,40 @@
+﻿using ClassLib;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+
+namespace BILibraryBLL
+{
+    public class QueryTest
+    {
+        //get connectionString to connect Database
+        ConnectionStringTest con = new ConnectionStringTest();
+
+        //query out in dataTable
+        public DataTable SQL1()
+        {
+            //create datatable format in 'dt'
+            DataTable dt = new DataTable();
+            //create connection to access database by OleDbConnection 
+            using (OleDbConnection thisConnection = new OleDbConnection(con.connection()))
+            {
+                string q = "Select * from CD_DAILY_DETAIL_FACT t Where rownum <= 100";
+                //prepare get q to use with thisconnection by command
+                OleDbCommand cmd = new OleDbCommand(q, thisConnection);
+                thisConnection.Open();
+                //Execute q 
+                OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+                //get result to fill in 'dt'
+                adapter.Fill(dt);
+
+                return dt;
+            }
+        }
+
+        
+
+    }
+}
---
+++ ClassLib/ConnectionStringTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public class ConnectionStringTest
+    {
+        public string connection()
+        {
+            String conn = "Data Source=(DESCRIPTION=" +
+                "(ADDRESS=(PROTOCOL=TCP)(HOST=192.168.41.101)(PORT=1521))" +
+                "(CONNECT_DATA=(SERVICE_NAME=EDBI)));" +
+                "User Id=ed_target;Password=oracle;Provider=OraOLEDB.Oracle;OLEDB.NET=True;";
+
+            return conn;
+        }
+    }
+}
