c2e830a1b26173baabaf05123453f06b9d59d6d9
Juthamart <Juthamart.k@v-smart.com>
2018-08-08 10:53:10 +0700

    add peemai brance

---
+++ BILibraryBLL/FollowTax.cs
@@ -0,0 +1,33 @@
+﻿using ClassLib;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+
+namespace BILibraryBLL
+{
+    public class FollowTax
+    {
+        Conn con = new Conn();
+
+        public DataTable FollowTaxMth()
+        {
+            DataTable dt = new DataTable();
+            OleDbConnection thisConnection = new OleDbConnection(con.connection());
+
+            string sql = "select o.region_name, sum(t.TAX_NETTAX_AMT) AS amt from Ic_Sum_Allday_Cube t,"+
+                           " Ic_Office_Dim o where t.offcode_own = o.offcode and "+
+                           " t.time_id between 20171001 and 20171031 group by o.region_name, o.region_cd "+
+                           " order by o.region_cd";
+            OleDbCommand cmd = new OleDbCommand(sql, thisConnection);  //EDIT : change table name for Oracle
+            thisConnection.Open();
+            OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+            adapter.Fill(dt);
+
+            return dt;
+
+        }
+    }
+}
---
+++ BIWebService/Controllers/FollowTaxController.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BILibraryBLL;
+using ClassLib;
+
+namespace BIWebService.Controllers
+{
+    public class FollowTaxController : ApiController
+    {
+        FollowTax fow = new FollowTax();
+        // GET: api/FollowTax
+        public IHttpActionResult Get()
+        {
+            var jsonString = JsonConvert.SerializeObject(fow.FollowTaxMth());
+            return new RawJsonActionResult(jsonString);
+        }
+
+        // GET: api/FollowTax/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/FollowTax
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/FollowTax/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/FollowTax/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
