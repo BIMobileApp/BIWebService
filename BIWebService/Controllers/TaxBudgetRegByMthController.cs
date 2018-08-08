805b53fb49699a4a4fa25ce66cf291605b1bdb16
Peemai124 <nongmai_kitchakoot@hotmail.com>
2018-08-08 11:44:58 +0700

    เพิ่ม api รายงานเปรียบเทียบภาษีปีก่อน

---
+++ BIWebService/Controllers/TaxBudgetRegByMthController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BILibraryBLL;
+using Newtonsoft.Json;
+
+namespace BIWebService.Controllers
+{
+    public class TaxBudgetRegByMthController : ApiController
+    {
+        TaxBudgetYear tax = new TaxBudgetYear();
+        // GET: api/TaxBudgetRegByMth
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/TaxBudgetRegByMth/5
+        public IHttpActionResult Get(int mth)
+        {
+            var jsonString = JsonConvert.SerializeObject(tax.TaxBudgetRegByMth(mth));
+            return new RawJsonActionResult(jsonString);
+        }
+
+        // POST: api/TaxBudgetRegByMth
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/TaxBudgetRegByMth/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/TaxBudgetRegByMth/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ BIWebService/Controllers/TaxBudgetYearController.cs
@@ -0,0 +1,43 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BILibraryBLL;
+
+namespace BIWebService.Controllers
+{
+    public class TaxBudgetYearController : ApiController
+    {
+        TaxBudgetYear tax = new TaxBudgetYear();
+        // GET: api/TaxBudgetYear
+        public IHttpActionResult Get()
+        {
+            var jsonString = JsonConvert.SerializeObject(tax.TaxBudgetOnYear());
+            return new RawJsonActionResult(jsonString);
+        }
+
+        // GET: api/TaxBudgetYear/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/TaxBudgetYear
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/TaxBudgetYear/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/TaxBudgetYear/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
