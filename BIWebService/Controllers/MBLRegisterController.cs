2032228ed04ab37779717e4c0f1e55b36017cc2f
Peemai124 <nongmai_kitchakoot@hotmail.com>
2018-08-22 12:09:32 +0700

    update query

--- BIWebService/Controllers/MBLRegisterController.cs
+++ BIWebService/Controllers/MBLRegisterController.cs
@@ -1,41 +1,41 @@
-﻿using System;
+﻿using BILibraryBLL;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Web.Http;
-using BILibraryBLL;
-using Newtonsoft.Json;
 
 namespace BIWebService.Controllers
 {
-    public class TaxBudgetRegController : ApiController
+    public class MBLRegisterController : ApiController
     {
-        TaxBudgetYear tax = new TaxBudgetYear();
-        // GET: api/TaxBudgetReg
+        MBLRegister tax = new MBLRegister();
+        // GET: api/MBLRegister
         public IEnumerable<string> Get()
         {
             return new string[] { "value1", "value2" };
         }
 
-        // GET: api/TaxBudgetReg/5
-        public IHttpActionResult Get(string offcode, string group_id, string year)
+        // GET: api/MBLRegister/5
+        public IHttpActionResult Get(string offcode)
         {
-            var jsonString = JsonConvert.SerializeObject(tax.TaxBudgetReg(offcode, group_id, year));
+            var jsonString = JsonConvert.SerializeObject(tax.TaxRegisterByOffcode(offcode));
             return new RawJsonActionResult(jsonString);
         }
 
-        // POST: api/TaxBudgetReg
+        // POST: api/MBLRegister
         public void Post([FromBody]string value)
         {
         }
 
-        // PUT: api/TaxBudgetReg/5
+        // PUT: api/MBLRegister/5
         public void Put(int id, [FromBody]string value)
         {
         }
 
-        // DELETE: api/TaxBudgetReg/5
+        // DELETE: api/MBLRegister/5
         public void Delete(int id)
         {
         }
