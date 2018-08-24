eabd1e2411e0fe5f3f31f988a6c6b2ed46d7c9ba
Juthamart <Juthamart.k@v-smart.com>
2018-08-24 11:05:54 +0700

    add law service finish

---
+++ BILibraryBLL/LawReport.cs
@@ -0,0 +1,44 @@
+﻿using ClassLib;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.OleDb;
+using System.Linq;
+using System.Web;
+
+namespace BILibraryBLL
+{
+    public class LawReport
+    {
+        Conn con = new Conn();
+        public DataTable LawReportArea(string offcode)
+        {
+            DataTable dt = new DataTable();
+            OleDbConnection thisConnection = new OleDbConnection(con.connection());
+
+            string sql = @"select * from MBL_LAW_REPORT_1 t where t.offcode ='"+ offcode+"'";
+
+            OleDbCommand cmd = new OleDbCommand(sql, thisConnection);  //EDIT : change table name for Oracle
+            thisConnection.Open();
+            OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+            adapter.Fill(dt);
+
+            return dt;
+        }
+
+        public DataTable LawReportMth(string offcode)
+        {
+            DataTable dt = new DataTable();
+            OleDbConnection thisConnection = new OleDbConnection(con.connection());
+
+            string sql = @"select TRANS_Short_month(t.budget_month_desc) as month, t.* from MBL_LAW_REPORT_2 t where t.offcode ='" + offcode+"'";
+
+            OleDbCommand cmd = new OleDbCommand(sql, thisConnection);  //EDIT : change table name for Oracle
+            thisConnection.Open();
+            OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+            adapter.Fill(dt);
+
+            return dt;
+        }
+    }
+}
---
+++ BIWebService/Controllers/LawReportMthController.cs
@@ -0,0 +1,21 @@
+﻿using BILibraryBLL;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BIWebService.Controllers
+{
+    public class LawReportMthController : ApiController
+    {
+        LawReport tax = new LawReport();
+        public IHttpActionResult Get(string offcode)
+        {
+            var jsonString = JsonConvert.SerializeObject(tax.LawReportMth(offcode));
+            return new RawJsonActionResult(jsonString);
+        }
+    }
+}
