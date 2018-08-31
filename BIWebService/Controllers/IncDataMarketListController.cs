005b03f7a25016883815b74868e5ecf42e1c1bbe
Peemai124 <nongmai_kitchakoot@hotmail.com>
2018-08-31 11:11:50 +0700

    sss

---
+++ BIWebService/Controllers/IncDataMarketListController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using BILibraryBLL;
+using Newtonsoft.Json;
+
+namespace BIWebService.Controllers
+{
+    public class IncDataMarketListController : ApiController
+    {
+        IncDataMarket tax = new IncDataMarket();
+
+        public IHttpActionResult Get(string offcode)
+        {
+            var jsonString = JsonConvert.SerializeObject(tax.IncDataMarketList(offcode));
+            return new RawJsonActionResult(jsonString);
+        }
+    }
+}
