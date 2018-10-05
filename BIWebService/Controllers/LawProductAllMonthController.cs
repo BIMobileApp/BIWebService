7045e53dc5df1dc1041980691dcdbe4e6e982ab5
Juthamart <Juthamart.k@v-smart.com>
2018-10-05 18:48:51 +0700

    aa

---
+++ BIWebService/Controllers/LawProductAllMonthController.cs
@@ -0,0 +1,22 @@
+﻿using BILibraryBLL;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BIWebService.Controllers
+{
+    public class LawProductAllMonthController : ApiController
+    {
+        LawReport tax = new LawReport();
+        // GET: api/LawProductByMthAll/5
+        public IHttpActionResult Get(string offcode, string region, string province, string month_from, string month_to)
+        {
+            var jsonString = JsonConvert.SerializeObject(tax.LawProductAllMonth(offcode, region, province, month_from, month_to));
+            return new RawJsonActionResult(jsonString);
+        }
+    }
+}
